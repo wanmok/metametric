684bda07ad00da2f1cd583440ea50ed9c3052372
Tongfei Chen <tongfei@protonmail.com>
2023-08-11 23:38:26 -0700

    remove networkx as a dependency

--- unimetric/core/metric.py
+++ unimetric/core/metric.py
@@ -2,9 +2,11 @@ from abc import abstractmethod
 from functools import reduce
 from operator import mul
 from typing import Dict, Generic, Set, Type, TypeVar
-import networkx as nx
+import numpy as np
+import scipy as sp
 
 T = TypeVar('T', contravariant=True)
+U = TypeVar('U')
 
 
 class Metric(Generic[T]):
@@ -39,26 +41,23 @@ class ProductMetric(Metric[T]):
         )
 
 
-class AlignmentMetric(Metric[Set[T]]):
-    def __init__(self, inner: Metric[T]):
+class AlignmentMetric(Metric[Set[U]]):
+    def __init__(self, inner: Metric[U]):
         self.inner = inner
 
-    def score(self, x: T, y: T) -> float:
-        # TODO(tongfei): if inner metric is discrete, use direct counting
-        g = nx.Graph()
-        g.add_nodes_from(((0, u) for u in x), bipartite=0)
-        g.add_nodes_from(((1, v) for v in y), bipartite=1)
-        g.add_weighted_edges_from(
-            ((0, u), (1, v), -self.inner.score(u, v))
+    def score(self, x: Set[U], y: Set[U]) -> float:
+        m = np.array([
+            [
+                self.inner.score(u, v)
+                for v in y
+            ]
             for u in x
-            for v in y
+        ])
+        row_idx, col_idx = sp.optimize.linear_sum_assignment(
+            cost_matrix=m,
+            maximize=True,
         )
-        matching = nx.bipartite.minimum_weight_full_matching(g)
-        return -sum(
-            g[(0, u)][matching[(0, u)]]['weight']
-            for u in x
-            if (0, u) in matching
-        )
+        return m[row_idx, col_idx].sum()
 
 
 class Jaccard(Metric[T]):
